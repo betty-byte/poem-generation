ba30f60ee2daaa96c6571b5c3e937e576af259b0
betty-byte <bettycup8@gmail.com>
2025-03-14 18:53:53 +0200

    Worked on the UX

--- script.js
+++ script.js
@@ -17,6 +17,11 @@ function generateapoem(event) {
     "You are a talented poem writer and like to write short poem on a wide rage of topics. Your mission is to write an eight line poem following the user instructions. Separate each line with a <br/> and add extra <br/> after every third line. Sign the poem with 'SheCodes AI' in <strong/>";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector(".actual-poem");
+  poemElement.classList.remove("hidden");
+
+  poemElement.innerHTML = `Generating your poem about ${poemSpecific.value} <div class="loader"></div>`;
+
   axios.get(apiUrl).then(poemResults);
 }
 
