f8168da5665b3d9e21ccd619de303f120f117851
betty-byte <bettycup8@gmail.com>
2025-03-14 06:15:12 +0200

    Added basic JS on the form input

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function generateapoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#actual-poem", {
+    strings: "This could be our poem but you are just not there",
+    autoStart: true,
+    cursor: "",
+    delay: 3,
+  });
+
+  actualPoem.innerHTML = "";
+}
+
+let poemFormElement = document.querySelector("#generator-form");
+poemFormElement.addEventListener("submit", generateapoem);
