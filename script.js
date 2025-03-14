8df1e850a3fc56654c04c0b6e621f712d3d888ea
betty-byte <bettycup8@gmail.com>
2025-03-14 14:58:36 +0200

    Added AI API intergration

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-function generateapoem(event) {
-  event.preventDefault();
-
+function poemResults(response) {
   new Typewriter("#actual-poem", {
-    strings: "This could be our poem but you are just not there",
+    strings: response.data.answer,
     autoStart: true,
     cursor: "",
     delay: 3,
   });
+}
 
-  actualPoem.innerHTML = "";
+function generateapoem(event) {
+  event.preventDefault();
+
+  let poemSpecific = document.querySelector(".describe-it");
+  let apiKey = "c2bo39f2at924270a681947bc398017a";
+  let prompt = `User instructions: Generate a poem about ${poemSpecific.value}`;
+  let context =
+    "You are a talented poem writer and like to write short poem on a wide rage of topics. Your mission is to write an eight line poem following the user instructions. Separate each line with a <br/> and add extra <br/> after every third line. Sign the poem with 'SheCodes AI' in <strong/>";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(poemResults);
 }
 
 let poemFormElement = document.querySelector("#generator-form");
